a306c52a580851b1706d2874ecf84c9c2c51d5b5
louisebochnia <louise.bochnia@gmail.com>
2025-10-09 09:21:45 -0300

    Fazendo as Configurações básicas do Flutter.

---
+++ atlas_digital/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  //para passar de tela em tela
+  var app = const MaterialApp(
+    home: Text('Hellooo, Flutter')
+  ); 
+  runApp(app);
+}
