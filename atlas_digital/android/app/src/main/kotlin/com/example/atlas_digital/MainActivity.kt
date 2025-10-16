c5756578c7ed00016a1f6e41bafb1088c5fa8994
louisebochnia <louise.bochnia@gmail.com>
2025-10-16 08:55:42 -0300

    Conexão inicial com o banco de dados

--- atlas_digital/android/app/src/main/kotlin/com/example/atlas_digital/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.atlas_digital
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- atlas_digital/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main(){
-  //para passar de tela em tela
-  var app = const MaterialApp(
-    home: Text('Hellooo, Flutter')
-  ); 
-  runApp(app);
-}
