7bd7f968960363ded603f90c35b59d3c365604cc
TheKazotskyKickGuy <89810421+TheKazotskyKickGuy@users.noreply.github.com>
2025-10-20 15:22:33 -0300

    excuir esquema subadmin
    Removed SubAdmin schema and related methods from app.js.

--- backendJavaScript/app.js
+++ backendJavaScript/app.js
@@ -6,45 +6,6 @@ const multer = require('multer');
 const path = require('path');
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
-// models/SubAdmin.js
-
-const SubAdminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Admin',
-    required: true
-  }
-});
-
-// Criptografa a senha antes de salvar
-SubAdminSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-SubAdminSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-module.exports = mongoose.model('SubAdmin', SubAdminSchema);
-
 
 const AdminSchema = new mongoose.Schema({
   username: {
