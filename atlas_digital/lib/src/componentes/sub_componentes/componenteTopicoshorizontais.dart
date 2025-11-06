b3612704a3da73e72ae2892b1b10a89421e57c48
LucasDeMattiaPeres <lucasperes@terra.com.br>
2025-11-06 19:20:20 -0300

    Create telaConteudo e secaoHorizontal

---
+++ atlas_digital/lib/src/componentes/sub_componentes/componenteTopicoshorizontais.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+class SecaoHorizontal extends StatelessWidget {
+  final String titulo;
+  final String descricao;
+  final List<Map<String, String>> itens;
+
+  const SecaoHorizontal({
+    super.key,
+    required this.titulo,
+    required this.descricao,
+    required this.itens,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = ScrollController(); // controla a rolagem
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Título da seção
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 12, 12, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                titulo,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                'Capítulos: 1-7',
+                style: const TextStyle(fontSize: 16, color: Colors.black87),
+              ),
+            ],
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 12, 12, 0),
+          child: Text(
+            descricao,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black87,
+              fontFamily: "Arial",
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 16),
+
+        // Carrossel horizontal com Scrollbar
+        SizedBox(
+          height: 190, // altura do card
+          child: Scrollbar(
+            controller: controller,
+            thumbVisibility: true,
+            trackVisibility: true,
+            thickness: 6,
+            radius: const Radius.circular(10),
+            child: ListView.builder(
+              controller: controller,
+              scrollDirection: Axis.horizontal,
+              itemCount: itens.length,
+              itemBuilder: (context, index) {
+                final item = itens[index];
+
+                return Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 24, 8),
+                  padding: const EdgeInsets.all(6),
+                  width: 240,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 231, 230, 230),
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Imagem
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network(
+                          item['url']!,
+                          height: 120,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+
+                      const SizedBox(height: 6),
+
+                      // Capítulo + título + botão
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Capítulo
+                          Text(
+                            'Capítulo ${item['capitulo']}',
+                            style: const TextStyle(
+                              fontSize: 10,
+                              color: Colors.black,
+                            ),
+                          ),
+
+
+                          // Título + botão
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  item['titulo']!,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black87,
+                                  ),
+                                  overflow:
+                                      TextOverflow.ellipsis, // evita estourar
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  print('Clicou no botão!');
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: const [
+                                    Text(
+                                      'Acessar',
+                                      style: TextStyle(
+                                        color: Color.fromARGB(
+                                          255,
+                                          170,
+                                          14,
+                                          170,
+                                        ),
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    SizedBox(width: 2),
+                                    Icon(
+                                      Icons.arrow_right_alt_sharp,
+                                      color: Color.fromARGB(255, 170, 14, 170),
+                                      size: 24,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ atlas_digital/lib/src/componentes/telaConteudo.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'sub_componentes/componenteTopicoshorizontais.dart';
+
+class telaConteudo extends StatefulWidget {
+  const telaConteudo({super.key});
+
+  @override
+  State<telaConteudo> createState() => _telaConteudoState();
+}
+
+class _telaConteudoState extends State<telaConteudo> {
+  // Simula buscar dados de um banco (poderia ser uma API real)
+  Future<List<Map<String, dynamic>>> buscarDadosDoBanco() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    ); // simula o tempo da requisição
+
+    return [
+      {
+        "titulo": "Lugares Recomendados",
+        "descricao":
+            "Essa galeria permite a navegação rápida pelas lâminas de microscópio em cada capítulo. Embora as lâminas não tenham descrições, você ainda pode identificar características individuais usando a lista suspensa no canto superior direito da imagem.",
+        "itens": [
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+        ],
+      },
+      {
+        "titulo": "Comidas Populares",
+        "descricao":
+            "Essa galeria permite a navegação rápida pelas lâminas de microscópio em cada capítulo. Embora as lâminas não tenham descrições, você ainda pode identificar características individuais usando a lista suspensa no canto superior direito da imagem.",
+        "itens": [
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+          {
+            "url": "https://picsum.photos/300/200?1",
+            "titulo": "Montanha",
+            "capitulo": "01",
+          },
+        ],
+      },
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Conteúdo',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Essa galeria permite a navegação rápida pelas lâminas de microscópio em cada capítulo. Embora as lâminas não tenham descrições, você ainda pode identificar características individuais usando a lista suspensa no canto superior direito da imagem.',
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            const SizedBox(height: 20),
+
+            // 🔹 FutureBuilder que carrega as seções
+            FutureBuilder<List<Map<String, dynamic>>>(
+              future: buscarDadosDoBanco(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 50),
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Erro ao carregar dados: ${snapshot.error}"),
+                  );
+                }
+
+                final secoes = snapshot.data ?? [];
+
+                return Column(
+                  children: secoes.map((secao) {
+                    return SecaoHorizontal(
+                      titulo: secao["titulo"],
+                      descricao: secao["descricao"],
+                      itens: List<Map<String, String>>.from(secao["itens"]),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
